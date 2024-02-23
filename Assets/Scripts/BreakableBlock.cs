ff27d2264febdf8116d755607b831e4c2a79e90b
Spyco03 <90427786+Spyco03@users.noreply.github.com>
2024-02-23 18:39:11 -0500

    Breakable Blocks

---
+++ Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BreakableBlock : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _hit;
+    [SerializeField]
+    private GameObject _object;
+
+    public float strength = 3f;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Break Blocks"))
+        {
+            if (strength <= 0f)
+            {
+                _hit.Invoke();
+            }
+            else
+            {
+                strength =- 1;
+                Spawn();
+            }
+            
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(_object, transform.position, Quaternion.identity);
+    }
+
+}
